86f4975b22ce8fe4d470de0d2be962d69c93fdcb
Chin Yuan Xi <derrickcyx@gmail.com>
2025-04-11 12:55:34 +0800

    Added footer component to ITasset page Removed buttons for profile page

--- resources/views/ProfilePage.blade.php
+++ resources/views/ProfilePage.blade.php
@@ -19,17 +19,8 @@
             <p><strong>Email:</strong> {{ $data->email }}</p>
         </div>
 
-        <div class="profile-buttons">
-            <!-- Previous User -->
-            @if ($prevUser)
-                <a href="{{ url('/ProfilePage/' . $prevUser->id) }}" class="prev-user">Previous</a>
-            @endif
 
-            <!-- Next User -->
-            @if ($nextUser)
-                <a href="{{ url('/ProfilePage/' . $nextUser->id) }}" class="next-user">Next</a>
-            @endif
-        </div>
+        <x-footer />
     </div>
 
 </body>
