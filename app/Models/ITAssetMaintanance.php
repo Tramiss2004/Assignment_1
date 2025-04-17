a571fde641d257f00b37504bbefa436729d1c147
Chin Yuan Xi <derrickcyx@gmail.com>
2025-04-17 21:36:06 +0800

    Merge branch 'master' of https://github.com/Tramiss2004/Assignment_1
    # Conflicts:
    #	routes/web.php

--- app/Models/ITAssetMaintanance.php
+++ app/Models/ITAssetMaintanance.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class ITAssetMaintanance extends Model
+class ITAssetMaintenance extends Model
 {
     use HasFactory;
 }
